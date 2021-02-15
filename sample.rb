148c519fe8aec6d1f29f4ce502d909fb4d899ef9
ayano kobayashi <ultrakobayashiayano@yahoo.co.jp>
2021-02-15 11:23:44 +0900

    インデント修正

--- sample.rb
+++ sample.rb
@@ -8,8 +8,8 @@ module Clockwork
   # ここに、「どれくらいの周期で処理をするのか？」というClockworkのメソッドを記述します
   # ここに、slackで一定時間ごとに送りたい処理と設定を書き込みます
   handler do |job|
-  client = SlackNotify::Client.new(webhook_url: ENV['SLACK_WEBHOOK_URL'])
-  client.notify("Hello!")
-end
+    client = SlackNotify::Client.new(webhook_url: ENV['SLACK_WEBHOOK_URL'])
+    client.notify("Hello!")
+  end
   every(3.minutes, 'less.frequent.job')
 end
